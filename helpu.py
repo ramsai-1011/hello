c595435cd5e218b3705f958b8b8b0404b8635f59
ramsai-1011 <78834950+ramsai-1011@users.noreply.github.com>
2021-05-04 19:34:22 +0530

    Add files via upload

---
+++ helpu.py
@@ -0,0 +1,18 @@
+def perm(x):
+    if len(x)==0:
+        return []
+    if len(x)==1:
+       # print(x)
+        return [x]
+    l=[]
+    for i in range(len(x)):
+        x1=x[i]
+        xs=x[:i]+x[i+1:]
+        print(xs)
+        for i in perm(xs):
+            #print(x1,i)
+            l.append([x1]+i)
+    return l
+l=[1,2,3]
+for i in perm(l):
+    print(i)
